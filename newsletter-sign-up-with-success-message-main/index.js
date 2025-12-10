24f4448eb0a43efa02a6e823fc24da7b59df1de2
cosmicmistake0 <nasirumubarak61@gmail.com>
2025-12-10 19:13:50 +0100

    completed both the error and success dom

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -11,15 +11,26 @@ const open = document.getElementById("open");
 const modal = document.getElementById("modal_container");
 const close = document.getElementById("close");
 const email = document.getElementById("email");
+const text = document.getElementById("modal_text");
+const error = document.getElementById("error");
 
 open.addEventListener("click", (e) => {
   e.preventDefault();
 
   if (email.checkValidity()) {
+    const submittedEmail = email.value;
+    text.textContent = `A confirmation email has been sent to ${submittedEmail}. Please open it
+            and click the button inside to confirm your subscription.`;
     modal.classList.add("show");
+    email.value = "";
   } else {
-    email.reportValidity();
+    error.classList.add("show");
+    email.classList.add("show");
   }
+});
+email.addEventListener("input", () => {
+  error.classList.remove("show");
+  email.classList.remove("show");
 });
 
 close.addEventListener("click", () => {
